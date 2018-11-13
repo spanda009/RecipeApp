c9d09107eae765e982e2aade99859ef9dea944f9
spanda009 <spanda.009@gmail.com>
2018-11-13 21:21:36 +0530

    commiting changes

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+recipes:Recipe[]=[
+  new Recipe('A Test Recipe','This is for test','https://hips.hearstapps.com/del.h-cdn.co/assets/17/34/2048x1365/gallery-1503418862-chicken-thighs-delish.jpg?resize=768:*')
+];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
