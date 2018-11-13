b710cb63ce479c71cb3430e26d14bda397f8f765
spanda009 <spanda.009@gmail.com>
2018-11-13 22:11:14 +0530

    Added event binding and property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter } from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -7,12 +7,18 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected=new EventEmitter<Recipe>();
 recipes:Recipe[]=[
   new Recipe('A Test Recipe','This is for test','https://hips.hearstapps.com/del.h-cdn.co/assets/17/34/2048x1365/gallery-1503418862-chicken-thighs-delish.jpg?resize=768:*')
 ];
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe :Recipe)
+  {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
